4a1279265b409ad655dfa6d5fb41a81f2110c196
LuxBlas <practicante.frontend@insalud.pe>
2025-11-07 16:24:56 -0500

    feat: Add number of Tatiana Lagos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -13,4 +13,6 @@ export const CONNECTED_PHONE_NUMBERS = [
   { name: "Fernando INB2B", number: "51969332494" },
   { name: "Jazmin Gutiérrez", number: "51934386825" },
   { name: "Lizeth Majino", number: "51934470960" },
+  { name: "Tatiana Lagos", number: "51934456845 " },
+
 ];
